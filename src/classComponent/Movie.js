3c4d365ce5af761c939e3a016718bcf6be88f309
jeevan <jeevanchadarla1998@gmail.com>
2021-03-07 07:44:35 +0530

    simple

--- src/classComponent/Movie.js
+++ src/classComponent/Movie.js
@@ -1,31 +1,23 @@
 import React, {Component} from 'react';
 import Movies from '../functComponent/Movies';
 import './styles.css'
+import movies from './movies'
 class Movie extends Component{
 
-    getMovies = (name, likes, banner) => {
-        return <Movies name={name} likes={likes} banner={banner} />
+    state= {...movies}
+
+    getMovies = (movie) => {
+        return <Movies name={movie.name} likes={movie.likes} banner={movie.banner} />
     }
 
     render(){
         return(
             <div className='movies-section'>
-            {this.getMovies('Avengers','320k','https://images-na.ssl-images-amazon.com/images/I/81ExhpBEbHL._SY445_.jpg')}
-            
-            {this.getMovies('Heist','880k','https://media1.popsugar-assets.com/files/thumbor/nenOyZ9yShhaU723TiDgOhRAUfQ/560x0:2000x1440/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/07/31/789/n/44498184/f1e7dd4c5f245b63376cd6.99549863_/i/money-heist-season-5-cast.jpg')}
-
-            {/* <Movies 
-                name='Heist'
-                likes='880k'
-                banner='https://media1.popsugar-assets.com/files/thumbor/nenOyZ9yShhaU723TiDgOhRAUfQ/560x0:2000x1440/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/07/31/789/n/44498184/f1e7dd4c5f245b63376cd6.99549863_/i/money-heist-season-5-cast.jpg'
-                hero='Professor'
-            /> */}
-            {/* <Movies 
-                name='Friends'
-                likes='10M'
-                banner= 'https://pyxis.nymag.com/v1/imgs/7b8/293/c01025df7559d43ad3ef939ec23f02eee8-30-friends-gossip-tk.rsquare.w1200.jpg'
-                hero='Joe Tribyani'
-            /> */}
+            {/* {this.getMovies(this.state.avengers.name, this.state.avengers.likes, this.state.avengers.banner)} */}
+            {/* {this.getMovies(this.state.heist.name, this.state.heist.likes, this.state.heist.banner)} */}
+            {this.getMovies(this.state.avengers)}
+            {this.getMovies(this.state.heist)}
+            {this.getMovies(this.state.crimes)}
             </div>
         )
         }
---
+++ src/classComponent/movies.js
@@ -0,0 +1,19 @@
+const movies = {
+    avengers:{
+        name: 'Avengers',
+        likes: 0,
+        banner: 'https://images-na.ssl-images-amazon.com/images/I/81ExhpBEbHL._SY445_.jpg'
+    },
+    heist: {
+        name: 'Money Heist',
+        likes: 0,
+        banner: 'https://media1.popsugar-assets.com/files/thumbor/nenOyZ9yShhaU723TiDgOhRAUfQ/560x0:2000x1440/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/07/31/789/n/44498184/f1e7dd4c5f245b63376cd6.99549863_/i/money-heist-season-5-cast.jpg'
+    },
+    crimes:{
+        name: 'Delhi crimes',
+        likes: 0,
+        banner: 'https://i1.wp.com/feminisminindia.com/wp-content/uploads/2019/04/DC.jpg?fit=875%2C583&ssl=1'
+    }
+}
+
+export default movies;
